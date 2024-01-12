b9f1291a3f9e0d83c913f2dc7fb1208fef9c30da
giorg22 <giorgibakhturidze2202@gmail.com>
2024-01-12 14:53:53 +0400

    Add project files.

---
+++ consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Messaging.EventHubs.Consumer;
+using System.Text;
+
+var connectionString = "Endpoint=sb://testeventhub99.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=eWrcfH3/K9J94Led8bVpfV+qi7ogOt2aU+AEhESu3tE=";
+var eventHubName = "hahhub";
+
+string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+
+// It is recommended that you cache the Event Hubs clients for the lifetime of your
+// application, closing or disposing when application ends.  This example disposes
+// after the immediate scope for simplicity.
+
+await using (var consumer = new EventHubConsumerClient(consumerGroup, connectionString, eventHubName))
+{
+    EventPosition startingPosition = EventPosition.Earliest;
+    string partitionId = (await consumer.GetPartitionIdsAsync()).First();
+
+    using var cancellationSource = new CancellationTokenSource();
+    cancellationSource.CancelAfter(TimeSpan.FromSeconds(45));
+
+    await foreach (PartitionEvent receivedEvent in consumer.ReadEventsFromPartitionAsync(partitionId, startingPosition, cancellationSource.Token))
+    {
+        // At this point, the loop will wait for events to be available in the partition.  When an event
+        // is available, the loop will iterate with the event that was received.  Because we did not
+        // specify a maximum wait time, the loop will wait forever unless cancellation is requested using
+        // the cancellation token.
+        string eventBody = Encoding.UTF8.GetString(receivedEvent.Data.Body.ToArray());
+        Console.WriteLine($"Received event from partition {receivedEvent.Partition.PartitionId}: {eventBody}");
+    }
+}
---
+++ producer/Program.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Messaging.EventHubs.Producer;
+using Azure.Messaging.EventHubs;
+
+var connectionString = "Endpoint=sb://testeventhub99.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=eWrcfH3/K9J94Led8bVpfV+qi7ogOt2aU+AEhESu3tE=";
+var eventHubName = "hahhub";
+
+// It is recommended that you cache the Event Hubs clients for the lifetime of your
+// application, closing or disposing when application ends.  This example disposes
+// after the immediate scope for simplicity.
+
+await using (var producer = new EventHubProducerClient(connectionString, eventHubName))
+{
+    using EventDataBatch eventBatch = await producer.CreateBatchAsync();
+
+    if ((!eventBatch.TryAdd(new EventData("First"))) ||
+        (!eventBatch.TryAdd(new EventData("Second"))))
+    {
+        throw new ApplicationException("Not all events could be added to the batch!");
+    }
+
+    await producer.SendAsync(eventBatch);
+}
