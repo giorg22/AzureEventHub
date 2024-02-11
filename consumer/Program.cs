3e0165eb1ef4d4b0ec5e33002c4889d31cd72dab
giorg22 <giorgibakhturidze2202@gmail.com>
2024-02-11 16:28:55 +0400

    bbb

--- consumer/Program.cs
+++ consumer/Program.cs
@@ -1,14 +1,14 @@
 ﻿using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+using Azure.Storage.Blobs;
 using System.Text;
+using Azure.Identity;
+using Azure.Messaging.EventHubs;
 
-var connectionString = "Endpoint=sb://testeventhub99.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=eWrcfH3/K9J94Led8bVpfV+qi7ogOt2aU+AEhESu3tE=";
-var eventHubName = "hahhub";
+var connectionString = "Endpoint=sb://namespaca.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=EMErWDVKjC/IGlOoX43RbZ1bNwCewnMo6+AEhP/f3/o=";
+var eventHubName = "bbb";
 
 string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
-
-// It is recommended that you cache the Event Hubs clients for the lifetime of your
-// application, closing or disposing when application ends.  This example disposes
-// after the immediate scope for simplicity.
 
 await using (var consumer = new EventHubConsumerClient(consumerGroup, connectionString, eventHubName))
 {
@@ -20,11 +20,43 @@ await using (var consumer = new EventHubConsumerClient(consumerGroup, connection
 
     await foreach (PartitionEvent receivedEvent in consumer.ReadEventsFromPartitionAsync(partitionId, startingPosition, cancellationSource.Token))
     {
-        // At this point, the loop will wait for events to be available in the partition.  When an event
-        // is available, the loop will iterate with the event that was received.  Because we did not
-        // specify a maximum wait time, the loop will wait forever unless cancellation is requested using
-        // the cancellation token.
         string eventBody = Encoding.UTF8.GetString(receivedEvent.Data.Body.ToArray());
         Console.WriteLine($"Received event from partition {receivedEvent.Partition.PartitionId}: {eventBody}");
     }
+}
+
+BlobContainerClient storageClient = new BlobContainerClient("DefaultEndpointsProtocol=https;AccountName=staccount55;AccountKey=Glki0PfVvpOycqkTNLXMchfNjW2axsyXvZYTjxn/mIOce0RO0jTIdomdyBqEAjRzTLv1xw5yxl5J+AStadRsuw==;EndpointSuffix=core.windows.net", "checkpointa");
+
+var processor = new EventProcessorClient(
+    storageClient,
+    EventHubConsumerClient.DefaultConsumerGroupName,
+    connectionString,
+    eventHubName);
+
+// Register handlers for processing events and handling errors
+processor.ProcessEventAsync += ProcessEventHandler;
+processor.ProcessErrorAsync += ProcessErrorHandler;
+
+// Start the processing
+await processor.StartProcessingAsync();
+
+// Wait for 30 seconds for the events to be processed
+await Task.Delay(TimeSpan.FromSeconds(60));
+
+// Stop the processing
+await processor.StopProcessingAsync();
+
+static async Task ProcessEventHandler(ProcessEventArgs eventArgs)
+{
+    // Write the body of the event to the console window
+    Console.WriteLine("\tReceived event: {0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
+    await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
+}
+
+Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+{
+    // Write details about the error to the console window
+    Console.WriteLine($"\tPartition '{eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
+    Console.WriteLine(eventArgs.Exception.Message);
+    return Task.CompletedTask;
 }
--- producer/Program.cs
+++ producer/Program.cs
@@ -1,19 +1,15 @@
 ﻿using Azure.Messaging.EventHubs.Producer;
 using Azure.Messaging.EventHubs;
 
-var connectionString = "Endpoint=sb://testeventhub99.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=eWrcfH3/K9J94Led8bVpfV+qi7ogOt2aU+AEhESu3tE=";
-var eventHubName = "hahhub";
-
-// It is recommended that you cache the Event Hubs clients for the lifetime of your
-// application, closing or disposing when application ends.  This example disposes
-// after the immediate scope for simplicity.
+var connectionString = "Endpoint=sb://namespaca.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=EMErWDVKjC/IGlOoX43RbZ1bNwCewnMo6+AEhP/f3/o=";
+var eventHubName = "bbb";
 
 await using (var producer = new EventHubProducerClient(connectionString, eventHubName))
 {
     using EventDataBatch eventBatch = await producer.CreateBatchAsync();
 
-    if ((!eventBatch.TryAdd(new EventData("First"))) ||
-        (!eventBatch.TryAdd(new EventData("Second"))))
+    if ((!eventBatch.TryAdd(new EventData("a"))) ||
+        (!eventBatch.TryAdd(new EventData("b"))))
     {
         throw new ApplicationException("Not all events could be added to the batch!");
     }
